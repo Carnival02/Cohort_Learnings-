9cfd2682c7d6acd8accf8dba69f87c9f7c73fd07
Harsh <carnivalking02@gmail.com>
2025-07-05 23:23:52 +0530

    something new

--- http_server/index.js
+++ http_server/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const bodyparser=require('body-parser')
 const app = express()
+
 const port = 3000
 
-app.get('/', function(req, res) {
-  res.send('Hello World! Mr Harsh Vardhan')
+app.use(bodyparser.json())
+app.post('/backendapi', (req, res) => {
+
+  const message=req.body.message;
+  console.log(message);
+  
+
+  res.json({
+    output:"2+2 = 4"
+  })
+
+
 })
 
-app.listen(port)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 
