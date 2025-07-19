472dba39ce44413d646e268279b1aa2b472e6557
Harsh <carnivalking02@gmail.com>
2025-07-19 11:42:09 +0530

    Backend / Express

--- week4/index2.js
+++ week4/index2.js
@@ -3,3 +3,20 @@
 // Read -> GET Request
 // Update -> Put Request
 // Delete -> DELETE Request
+
+
+const express=require("express")
+
+//starting app 
+const app=express()
+ 
+// Route handler
+app.get('/',function(req,res){
+    res.send('Hello world')
+})
+app.get('/asd',function(req,res){
+    res.send('Hello world from ASD')
+})
+
+app.listen(3000) // Port Allocation
+
