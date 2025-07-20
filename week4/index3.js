d72763a6b1759143364f0cf957b981b8082e5b23
Harsh <carnivalking02@gmail.com>
2025-07-20 11:06:40 +0530

    all about Middlewares

--- week4/index3.js
+++ week4/index3.js
@@ -3,3 +3,59 @@
 // app.get('/',function(req,res){
 //     const num=req.query.n;
 // })
+
+const express=require("express");
+
+
+const app=express();
+
+//Ticket Checker
+// function isOldAge(age){
+//     if(age>=14){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+
+//app.use(isOldAgeMiddleWare) --> if this middleware use in every route
+
+function isOldAgeMiddleWare(req,res,next){
+    const age=req.query.age;
+    if(age>=14){
+        next();
+    }
+    else{
+        res.json({
+            msg:"You are not Enough",
+        })
+    }
+}
+
+
+
+
+
+app.get('/ride',isOldAgeMiddleWare,function(req,res){
+    // isOldAgeMiddleWare();
+    res.json({
+        msg:"You have successfully riden the ride 1"
+    })
+    
+    
+
+   
+})
+
+app.get('/ride2',isOldAgeMiddleWare,function(req,res){
+   
+    res.json({
+        msg:"You have successfully riden the ride 2"
+    })
+    
+
+   
+})
+
+app.listen(3000);
