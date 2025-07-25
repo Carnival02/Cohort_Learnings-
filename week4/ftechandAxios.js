ad7afd42186c06a69a01245db03209ace3c96c6b
Harsh <carnivalking02@gmail.com>
2025-07-25 08:41:56 +0530

    about Fetch and Axios

---
+++ week4/ftechandAxios.js
@@ -0,0 +1,20 @@
+
+
+// AXIOS vs FETCH
+
+
+// --> FETCH APis
+
+// function main(){
+    // fetch("URL")
+    // .then(async response=>{
+        // const json=await response.json();
+        // console.log(json);
+// 
+        // 
+    // })
+// }
+// main()
+
+const axios =require("axios")
+
