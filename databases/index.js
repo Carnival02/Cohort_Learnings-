425b883ea272808f0ec4b782e4055116d547af41
Harsh <carnivalking02@gmail.com>
2025-08-07 15:46:46 +0530

    Updated index and adding Auth.js

--- databases/index.js
+++ databases/index.js
@@ -1,22 +1,47 @@
 const express = require("express");
+const bcrypt=require('bcrypt');
 const { UserModel, TodoModel } = require("./db.js"); // importing the db.js so that can create it 
-// const { auth, JWT_SECRET } = require("./auth");
+const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
-const JWT_SECRET="ghagjdhuj6466"
+
+const { z } =require("zod");
+// const JWT_SECRET="ghagjdhuj6ghh466"
 mongoose.connect("mongodb+srv://carnival:7532040743H%40r@cluster0.p5ihj9o.mongodb.net/")
 
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async function(req, res) {
+
+/// Validating the email and password 
+    const requireBody=z.object({
+        email:z.string().min(3).max(100),
+        name:z.string(),
+        password:z.string()
+    })
+
+    // const parsedData=requireBody.parse(req.body);
+    const parsedData=requireBody.safeParse(req.body);//--> safe parse 
+
+    if(!parsedData.success){
+        res.json({
+            message:"Inncorrect formaat"
+        })
+        return
+    }
+
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
+    const hasedPassword=await bcrypt.hash(password,5); //-> hashsiing the password
+    console.log(hasedPassword);
+    
+
     await UserModel.create({ // this is inserting in db-> server in india kolkata 
         email: email,
-        password: password,
+        password: hasedPassword,
         name: name
     });
     
@@ -32,10 +57,19 @@ app.post("/signin", async function(req, res) {
 
     const response = await UserModel.findOne({
         email: email,
-        password: password,
+        // password: password,
     });
 
-    if (response) {
+    if(!response){
+        res.status(403).json({
+            message:"user does not exists in our db"
+        })
+    }
+
+    const passwordMatch=await bcrypt.compare(password,response.password);
+
+
+    if (passwordMatch) {
         const token = jwt.sign({
             id: response._id.toString() // sending token in objectId 
         }, JWT_SECRET);
@@ -49,6 +83,7 @@ app.post("/signin", async function(req, res) {
         })
     }
 });
+
 
 
 app.post("/todo",  async function(req, res) {
